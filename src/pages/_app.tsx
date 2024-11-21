b54ccbedc111d08dec62dabd5cc42de63dbca005
thiagohrcosta <thiagohrcosta86@gmail.com>
2024-11-21 19:37:38 -0300

    Adding header and styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,21 @@
 import { globalStyles } from "@/styles/global";
 import type { AppProps } from "next/app";
 
+import logoImg from '../assets/logo.svg';
+import { Container, Header } from "@/styles/pages/app";
+
 globalStyles();
 
 function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Container>
+      <Header>
+        <img src={logoImg.src} alt="Logo" />
+      </Header>
+
+      <Component {...pageProps} />
+    </Container>
+  )
 }
 
 export default App;
